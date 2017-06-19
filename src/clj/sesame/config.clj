35dd674a9adc4ea8d7191237710e5cf8ae969578
Arne Brasseur <arne@arnebrasseur.net>
2017-06-19 17:10:56 +0200

    Remove ring-middleware-logger so datomic works

--- src/clj/sesame/config.clj
+++ src/clj/sesame/config.clj
@@ -1,11 +1,9 @@
 (ns sesame.config
   (:require [environ.core :refer [env]]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [ring.middleware.gzip :refer [wrap-gzip]]
-            [ring.middleware.logger :refer [wrap-with-logger]]))
+            [ring.middleware.gzip :refer [wrap-gzip]]))
 
 (defn config []
   {:http-port  (Integer. (or (env :port) 10555))
    :middleware [[wrap-defaults api-defaults]
-                wrap-with-logger
                 wrap-gzip]})
